8b25413af518c213d1de3ed0cb63048f026022c5
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-09 15:05:48 +0300

    Update CORS configuration to correct the production origin for socket connections

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -9,7 +9,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://chat-app-frontend-qc18.onrender.com/api",
+      "https://chat-app-frontend-qc18.onrender.com",
     ],
   },
 });
