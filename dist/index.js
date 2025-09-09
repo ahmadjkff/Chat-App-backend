4a8af4e4e260496722e660d82b2290f7b9f9c1a5
Ahmad Jamil Alfakori <ahmadjkff1@gmail.com>
2025-09-09 17:44:54 +0300

    2Refactor CORS configuration to use a single production origin and update cookie settings for cross-site compatibility

--- dist/index.js
+++ dist/index.js
@@ -12,21 +12,12 @@ const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const cors_1 = __importDefault(require("cors"));
 const socket_1 = require("./lib/socket");
 const PORT = process.env.PORT || 5001;
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://chat-app-frontend-qc18.onrender.com",
-];
 dotenv_1.default.config();
 socket_1.app.use(express_1.default.json({ limit: "40mb" })); // default 100kb
 socket_1.app.use(express_1.default.urlencoded({ limit: "40mb", extended: true }));
 socket_1.app.use((0, cookie_parser_1.default)());
 socket_1.app.use((0, cors_1.default)({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin))
-            callback(null, true);
-        else
-            callback(new Error("Not allowed by CORS"));
-    },
+    origin: "https://chat-app-frontend-qc18.onrender.com",
     credentials: true,
 }));
 socket_1.app.use("/api/auth", auth_route_1.default);
